45be328f8348b88e92f0b6f7387456afceca9ca7
anushree-0805 <anushree@mkrypt.org>
2025-09-30 15:13:28 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className= 'flex justify-center items-center h-screen'>
+      <Button>Click Me</Button>
+      
+    </div>
+  )
+}
+
+export default page
